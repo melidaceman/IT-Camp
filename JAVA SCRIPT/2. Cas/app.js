a6abe1c1426c2df9782f4a6126376814e8839213
melidaceman <melidaceman@gmail.com>
2023-03-25 11:35:43 +0100

    Tipovi podataka

---
+++ JAVA SCRIPT/2. Cas/app.js
@@ -0,0 +1,13 @@
+// 3. Set - Struktura podataka za skladistenje vise vrednosti koja ne dozvoljava duplikate. Slicna je nkizu (Array), a sintaksa za pravljenje seta je:
+
+set = new Set(["Asija", "Melida", "Dino", 19, 31, 25, "Dino"]); // Svako novo pojavljivanje elementa u setu se nece primeniti.
+console.log(set);
+
+// 4. Map - Struktura podataka za skladistenje vise vrednosti i slicna je objektu. Sintaksa za pravljenje mape je:
+map = new Map([
+  ["banane", 160],
+  ["jagoda", 240],
+  ["kruska", 140],
+  ["ananas", 289],
+]);
+console.log(map);
