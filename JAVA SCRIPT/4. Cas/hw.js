7b3801b68447c11b6e23fcae024c8ee35ae6fe24
melidaceman <melidaceman@gmail.com>
2023-04-01 03:57:29 +0200

    Task js

---
+++ JAVA SCRIPT/4. Cas/hw.js
@@ -0,0 +1,11 @@
+const brojGodina = 40;
+
+if (brojGodina < 12) {
+  console.log("Vi ste decijeg doba");
+} else if (brojGodina >= 12 && brojGodina < 18) {
+  console.log("Vi ste maloletni");
+} else if (brojGodina >= 18 && brojGodina < 40) {
+  console.log("Vi ste punoletni");
+} else if (brojGodina >= 40) {
+  console.log("Vi ste zrela osoba");
+}
