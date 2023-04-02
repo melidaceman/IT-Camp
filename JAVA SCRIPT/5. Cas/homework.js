6a4c3193b06dd75e7418623171952db3f623433f
melidaceman <melidaceman@gmail.com>
2023-04-02 12:29:28 +0200

    Zadaci

--- JAVA SCRIPT/5. Cas/homework.js
+++ JAVA SCRIPT/5. Cas/homework.js
@@ -1,4 +1,4 @@
-// 1.
+// 1;
 // const x = 8;
 // const y = 7;
 
@@ -10,14 +10,33 @@
 
 // 2.
 
-x = 8;
-y = 0;
-const kolicnik = y;
-switch (kolicnik) {
-  case 4:
-    console.log(x / y);
-    break;
+// x = 24;
+// y = 4;
+// const kolicnik = y;
+// switch (kolicnik) {
+//   case 4:
+//     console.log(x / y);
+//     break;
 
+//   default:
+//     console.log("Deljenje je nemoguce");
+// }
+
+// const br1 = +prompt("Unesite prvi broj:");
+// const br2 = +prompt("Unesite drugi broj:"); // = Number(prompt ("Unesite drugi broj:")); mozemo raditi na oba nacina.
+
+// if (isNaN(br1) || isNaN(br2)) {
+// }
+// console.log("Uneti brojevi moraju biti ispravni");...
+
+// 2.
+const x = +prompt("Unesite prvi broj:");
+const y = Number(prompt("Unesite drugi broj:"));
+
+switch (y) {
+  case 0:
+    console.log("Deljenje nulom nije moguce.");
+    break;
   default:
-    console.log("Deljenje je nemoguce");
+    console.log("Kolicnik unetih brojeva je: " + x / y);
 }
