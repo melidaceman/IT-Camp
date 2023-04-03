8744b4972c69c08fd13f917ae901a3d667cf72d4
melidaceman <melidaceman@gmail.com>
2023-04-04 00:25:19 +0200

    Zadaci

---
+++ JAVA SCRIPT/6. Cas/math.js
@@ -0,0 +1,36 @@
+// 1.
+// Neparni brojevi od 1-20:
+//1. nacin:
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 != 0) {
+//     console.log(i);
+//   }
+// }
+
+// 2. nacin:
+// for (let i = 1; i < 21; i += 2) {
+//   console.log(i);
+// }
+
+// 2.
+// 50-100 koji su deljivi sa 5:
+
+// for (let i = 50; i <= 100; i++) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 4.
+// Suma prirodnih neparnih brojeva 10-20:
+
+let suma = 0;
+for (let i = 10; i <= 20; i++) {
+  if (i % 2 != 0) {
+    suma += i;
+  }
+  {
+    console.log(suma);
+  }
+}
