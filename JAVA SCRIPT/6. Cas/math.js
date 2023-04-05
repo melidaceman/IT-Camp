29858777d4e0162008c11a068326267ac196246a
melidaceman <melidaceman@gmail.com>
2023-04-05 23:00:46 +0200

    Zadaci vezbanje

--- JAVA SCRIPT/6. Cas/math.js
+++ JAVA SCRIPT/6. Cas/math.js
@@ -9,14 +9,14 @@
 // }
 
 // 2. nacin:
-// for (let i = 1; i < 21; i += 2) {
+// for (let i = 1; i < 21; i += 2) // i += 2 (i + 2){
 //   console.log(i);
 // }
 
 // 2.
 // 50-100 koji su deljivi sa 5:
 
-// for (let i = 50; i <= 100; i++) {
+// for (let i = 50; i <= 100; i++) // i++ = i se povecava za 1{
 //   if (i % 5 === 0) {
 //     console.log(i);
 //   }
@@ -35,8 +35,9 @@
 //   }
 // }
 
-for (let i = 10; i <= 20; i += 2) {
-  if (i % 2 != 0) {
-    console.log(i);
-  }
+// 2. nacin:
+let zbir = 0; //const ne menja vrednost i zato koristimo let
+for (let i = 11; i < 20; i += 2) {
+  zbir += i;
 }
+console.log(zbir);
