39a7f2a5d061fbfd752a251bf6faed526edd061c
melidaceman <melidaceman@gmail.com>
2023-04-28 12:59:22 +0200

    "While" assignments

--- JAVA SCRIPT/10. Cas/index.js
+++ JAVA SCRIPT/10. Cas/index.js
@@ -7,18 +7,28 @@
 // s - sirina terena u metrima(45 < s < 90)
 // r - rastojanje ograde od terena u metrima (2 < r < 10)
 
-// const s = +prompt("Unesite sirinu terena: ");
-// const d = +prompt("Unesite duzinu terena: ");
-// const r = +prompt("Unesite rastojanje terena");
-
-// const duzinaOgrade = 2 * s + 2 * d + 8 * r;
-// console.log("Treba nam " + duzinaOgrade + " metara za ogradjivanje terena");
+const s = +prompt("Unesite sirinu terena: ");
+const d = +prompt("Unesite duzinu terena: ");
+const r = +prompt("Unesite rastojanje terena");
+if (s < 45 || s > 90) {
+  console.log("Uneli ste neodgovarajucu sirinu.");
+} else if (d < 90 || d > 120) {
+  console.log("Uneli ste neodgovarajucu duzinu.");
+} else if (r < 2 || r > 10) {
+  console.log("Uneli ste neodgovarajuce rastojanje.");
+} else if (isNaN(s) || isNaN(d) || isNaN(r)) {
+  console.log("Uneli ste neodgovarajuce vrednosti.");
+} else {
+  console.log("Uneli ste netacne vrednosti.");
+}
+const duzinaOgrade = 2 * s + 2 * d + 8 * r;
+console.log("Treba nam " + duzinaOgrade + " metara za ogradjivanje terena");
 
 // Osnove o stringovima:
-const recenica = "Necemo matematicke zadatke!";
-console.log(recenica);
-console.log(recenica[0]); // u kvadratnim zagradama zapisujemo broj karaktera koji zelimo da promenimo
-console.log(recenica[7]);
+// const recenica = "Necemo matematicke zadatke!";
+// console.log(recenica);
+// console.log(recenica[0]); // u kvadratnim zagradama zapisujemo broj karaktera koji zelimo da promenimo
+// console.log(recenica[7]);
 
 // Pristupanje nekom karakteru ide preko indeksa!
 // Indeksiranje ide od 0!
@@ -26,16 +36,27 @@ console.log(recenica[7]);
 
 //  Za izracunavanje duzine nekog stringa se koristi length metoda:
 
-const duzina = recenica.length;
-console.log(duzina);
+// const duzina = recenica.length;
+// console.log(duzina);
 
 // Ispisivanje poslednjeg karatktera stringa:
-console.log(recenica[duzina - 1]);
-console.log(recenica[recenica.length - 1]);
+// console.log(recenica[duzina - 1]);
+// console.log(recenica[recenica.length - 1]);
 
 //Domaci zadaci:
 // 1.Zavrsiti zadatak za rastojanje sa uslovima
 
 // 2. Korisnik unosi broj iz intervala (12,16).
 // Na osnovu unetog broja,iteracija se vrsi od njega do 1 (ukljucujuci).
-// Ispisati svaki broj iz iteracije,ngegov kvadrat i vrednost broja umanjenu za 25.
+// Ispisati svaki broj iz iteracije,njegov kvadrat i vrednost broja umanjenu za 25.
+
+// let broj = +prompt("Unesi broj 12-16: ");
+
+// if (broj >= 12 && broj <= 16) {
+//   while (broj >= 1) {
+//     console.log(broj ** 2 - 25);
+//     broj--;
+//   }
+// } else {
+//   console.log("Uneli ste pogresnu vrednost.");
+// }
