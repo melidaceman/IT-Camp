d1b1cbeb4a358dabaea56228e81d418628c3354f
melidaceman <melidaceman@gmail.com>
2023-04-28 13:19:24 +0200

    Update

--- JAVA SCRIPT/9. Cas/index.js
+++ JAVA SCRIPT/9. Cas/index.js
@@ -32,33 +32,32 @@
 // }
 
 // 1. Ispisati sve brojeve od 1 do 10 koji su parni. (koristeci while petlju)
-// 2. Korisnik unosi 2 broja:
-// Na osnovu toga koji je broj manji,iteracija se vrsi od njega do veceg broja. Ispisuju se brojevi od manjeg ka vecem.
 
-let broj1 = +prompt("Unesite prvi broj: ");
-let broj2 = +prompt("Unesite drugi broj: ");
-if (isNaN(broj1) || isNaN(broj2)) {
-  console.log("Niste uneli korektne vrednosti.");
-} else if (broj1 < broj2) {
-  while (broj1 <= broj2) {
-    console.log(broj1);
-    broj1++;
+let i = 1;
+
+while (i <= 10) {
+  if (i % 2 === 0) {
+    console.log(i);
   }
-} else if (broj2 < broj1) {
-  while (broj2 <= broj1) {
-    console.log(broj2);
-    broj2++;
-  }
-} else {
-  console.log("Uneli ste iste brojeve.");
+  i++;
+  // 2. Korisnik unosi 2 broja:
+  // Na osnovu toga koji je broj manji,iteracija se vrsi od njega do veceg broja. Ispisuju se brojevi od manjeg ka vecem.
+
+  // let broj1 = +prompt("Unesite prvi broj: ");
+  // let broj2 = +prompt("Unesite drugi broj: ");
+  // if (isNaN(broj1) || isNaN(broj2)) {
+  //   console.log("Niste uneli korektne vrednosti.");
+  // } else if (broj1 < broj2) {
+  //   while (broj1 <= broj2) {
+  //     console.log(broj1);
+  //     broj1++;
+  //   }
+  // } else if (broj2 < broj1) {
+  //   while (broj2 <= broj1) {
+  //     console.log(broj2);
+  //     broj2++;
+  //   }
+  // } else {
+  //   console.log("Uneli ste iste brojeve.");
+  // }
 }
-
-// 1.
-// let k = 1;
-// while (k <= 10) {
-//   if (k % 2 === 0) {
-//     console.log(k);
-//   }
-// }
-
-// 2.
